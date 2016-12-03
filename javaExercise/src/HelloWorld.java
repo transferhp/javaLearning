b02574b4623892ec59788b7848cbb908d6aad1b7
Peng Hao <peng.hao@student.uts.edu.au>
2016-12-03 15:09:14 +1100

    Hello world practice

---
+++ javaExercise/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	/*
+	 * First, let's try classic program 'hello world!' with java.
+	 * 
+	 * Print 'Hello World! Hello Java!'
+	 */
+	
+	public static void main(String args[]){
+		System.out.println("Hello World! Hello Java!");
+	}
+
+}
